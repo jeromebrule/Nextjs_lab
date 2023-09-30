9f734b05e16f2bc2feae4337ef7142920b726aaf
jeromeVeritree <jerome.brule@veritree.com>
2023-09-30 10:25:15 -0700

    Update UserTable.tsx

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -22,27 +22,19 @@ interface Props {
 }
 
 const UserTable = ({sortOrder}: Props) => {
-  // const res = await fetch("https://jsonplaceholder.typicode.com/404");
-
   const [users, setUsers] = useState<Contact[]>([]);
   const [error, setError] = useState<any>();
 
   const fetchData = async () => {
-    setError("");
     try {
       const response = await fetch("http://localhost:3000/api/users/");
       // const response = await fetch("https://jsonplaceholder.typicode.com/404");
       if (!response.ok) {
-        // If the API responds meaningful error message,
-        // then you can get it by calling response.statusText
         throw new Error("Sorry something went wrong");
       }
       const data = await response.json();
       setUsers(data);
-    } catch (error) {
-      // It is always recommended to define the error messages
-      // in the client side rather than simply relying on the server messages,
-      // since server messages might not make sense to end user most of the time.
+    } catch (error: any) {
       setError(error.message);
     }
   };
