7399efd91fd0b473ee86ab6b83390bb60ce2bcf2
jeromeVeritree <jerome.brule@veritree.com>
2023-09-30 10:48:04 -0700

    Update not-found.tsx

--- app/users/[id]/not-found.tsx
+++ app/users/[id]/not-found.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from "next/link";
 
 const UserNotFoundPage = () => {
   return (
-    <div>This user doesn&apos;t exist.</div>
-  )
-}
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+};
 
-export default UserNotFoundPage
+export default UserNotFoundPage;
