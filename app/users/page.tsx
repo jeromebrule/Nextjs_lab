e41143e0760f46970cc09bd98d4175522599a48d
jeromeVeritree <jerome.brule@veritree.com>
2023-09-30 14:22:54 -0700

    Updates

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -11,10 +11,11 @@ interface Props {
 const UsersPage = ({searchParams: {sortOrder}}: Props) => {
   return (
     <>
-      <h1>Contacts</h1>
-      <Link href="/users/new" className="btn">
+      <h1 className="mb-5">Contacts</h1>
+      <Link href="/users/new" className="btn btn-primary">
         Create contact
       </Link>
+      <div className="divider"></div>
       <UserTable sortOrder={sortOrder} />
     </>
   );
