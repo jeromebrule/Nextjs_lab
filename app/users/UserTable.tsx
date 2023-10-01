9db7a1094ffad5bcdd0b174c618214a9c4f10226
jeromeVeritree <jerome.brule@veritree.com>
2023-09-30 18:12:35 -0700

    Update UserTable.tsx

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -4,6 +4,7 @@ import React, {useEffect, useState} from "react";
 import Link from "next/link";
 import {sort} from "fast-sort";
 import {RiDeleteBin5Line, RiEdit2Line} from "react-icons/ri";
+import {useSession} from "next-auth/react";
 
 interface Contact {
   id: number;
@@ -23,14 +24,15 @@ interface Props {
 }
 
 const UserTable = ({sortOrder}: Props) => {
+  const {data: session, status} = useSession();
+
   const [users, setUsers] = useState<Contact[]>([]);
   const [error, setError] = useState<any>();
-  const [status, setStatus] = useState<any>();
+  const [apistatus, setapiStatus] = useState<any>();
 
   const fetchData = async () => {
     try {
       const response = await fetch("http://localhost:3000/api/users/");
-      // const response = await fetch("https://jsonplaceholder.typicode.com/404");
       if (!response.ok) {
         throw new Error("Sorry something went wrong");
       }
@@ -48,10 +50,9 @@ const UserTable = ({sortOrder}: Props) => {
   const handleDeleteContact = (id: number) =>
     fetch(`http://localhost:3000/api/users/${id}`, {
       method: "DELETE",
-      body: JSON.stringify(id),
     })
       .then(function (response) {
-        setStatus(response.status);
+        setapiStatus(response.status);
         fetchData();
       })
       .catch(function (error) {
@@ -60,9 +61,9 @@ const UserTable = ({sortOrder}: Props) => {
 
   const handleEditContact = (id: number) => console.log(id);
 
-  // const sortedUsers = sort(users).asc(
-  //   sortOrder === "email" ? (user) => user.userEmail : (user) => user.userName
-  // );
+  const sortedUsers = sort(users).asc(
+    sortOrder === "email" ? (user) => user.userEmail : (user) => user.userName
+  );
 
   return (
     <table className="table table-bordered">
@@ -81,7 +82,7 @@ const UserTable = ({sortOrder}: Props) => {
         </tr>
       </thead>
       <tbody>
-        {users.map((user, index) => (
+        {sortedUsers.map((user, index) => (
           <tr key={user.id}>
             <td>
               <Link href={`/users/${encodeURIComponent(user.id)}`}>
@@ -93,26 +94,40 @@ const UserTable = ({sortOrder}: Props) => {
             <td>{user.userWebsite}</td>
             <td>{user.userCompanyName}</td>
             <td>
-              <div className="tooltip tooltip-info" data-tip="edit contact">
-                <button
-                  className="btn btn-circle btn-sm"
-                  onClick={() => handleEditContact(user.id)}
-                >
-                  <RiEdit2Line color="#000" size={20} />
-                </button>
-              </div>
-              <div className="tooltip tooltip-error" data-tip="delete contact">
-                <button
-                  className="ml-3 btn btn-circle btn-sm"
-                  onClick={() => handleDeleteContact(user.id)}
-                >
-                  <RiDeleteBin5Line color="#000" size={20} />
-                </button>
-              </div>
+              {status === "authenticated" && (
+                <>
+                  <div className="tooltip tooltip-info" data-tip="edit contact">
+                    <button
+                      className="btn btn-circle btn-sm"
+                      onClick={() => handleEditContact(user.id)}
+                    >
+                      <RiEdit2Line color="#000" size={20} />
+                    </button>
+                  </div>
+                  <div
+                    className="tooltip tooltip-error"
+                    data-tip="delete contact"
+                  >
+                    <button
+                      className="ml-3 btn btn-circle btn-sm"
+                      onClick={() => handleDeleteContact(user.id)}
+                    >
+                      <RiDeleteBin5Line color="#000" size={20} />
+                    </button>
+                  </div>
+                </>
+              )}
             </td>
           </tr>
         ))}
-        {status === 200 && (
+        {apistatus === 401 && (
+          <div className="toast toast-end">
+            <div className="alert alert-error">
+              <span>Unauthorized.</span>
+            </div>
+          </div>
+        )}
+        {apistatus === 200 && (
           <div className="toast toast-end">
             <div className="alert alert-info">
               <span>Contact Deleted.</span>
